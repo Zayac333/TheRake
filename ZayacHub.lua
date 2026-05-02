d0cabbf26595d05957409b78057a544409e21d60
Zayac333 <superzayac333@gmail.com>
2026-05-02 16:18:06 +0300

    Update library URL and author label in ZayacHub

---
+++ ZayacHub.lua
@@ -0,0 +1,512 @@
+local fenv = getfenv()
+local _LocalPlayer4 = game:service('Players').LocalPlayer
+
+game:service('Lighting')
+
+local _call12 = loadstring(game:HttpGet('https://raw.githubusercontent.com/Zayac333/TheRake/main/Librare.lua'))().new(true, 'Zayac Hub | The Rake Remastered')
+
+_call12.ChangeToggleKey(Enum.KeyCode.Insert)
+
+local _call20 = _call12:Category('Home'):Sector('Home')
+
+_call20:Cheat('Label', 'Thanks for using The Rake GUI by Zayac!')
+_call20:Cheat('Label', 'Insert to Hide/Show')
+
+local _call26 = _call12:Category('Main')
+local _call28 = _call26:Sector('LocalPlayer')
+
+spawn(function(...)
+    newcclosure(function(...)
+        local _31_vararg1 = ...
+        local _ = _31_vararg1 == _LocalPlayer4.Character:FindFirstChildWhichIsA('Humanoid')
+        local _ = _31_vararg1 == _LocalPlayer4.Character:FindFirstChildWhichIsA('Humanoid')
+        local _ = _31_vararg1 == workspace.CurrentCamera
+        local _ = _31_vararg1 == _LocalPlayer4
+
+        error('line 1: attempt to call a nil value')
+    end)
+    hookmetamethod(game, '__newindex', function(...)
+        local _44_vararg1 = ...
+        local _ = _44_vararg1 == _LocalPlayer4.Character:FindFirstChildWhichIsA('Humanoid')
+        local _ = _44_vararg1 == _LocalPlayer4.Character:FindFirstChildWhichIsA('Humanoid')
+        local _ = _44_vararg1 == workspace.CurrentCamera
+        local _ = _44_vararg1 == _LocalPlayer4
+
+        error('line 1: attempt to call a nil value')
+    end)
+end)
+_call28:Cheat('Slider', 'WalkSpeed', function(...) end, {
+    max = 30,
+    min = 0,
+    suffix = '',
+})
+_call28:Cheat('Slider', 'JumpPower', function(...) end, {
+    max = 100,
+    min = 0,
+    suffix = '',
+})
+_call28:Cheat('Slider', 'Gravity', function(...)
+    local _64_vararg1 = ...
+
+    workspace.Gravity = _64_vararg1
+end, {
+    max = 196,
+    min = 0,
+    suffix = '',
+})
+_call28:Cheat('Slider', 'FOV', function(...) end, {
+    max = 120,
+    min = 70,
+    suffix = '',
+})
+_call28:Cheat('Checkbox', 'No Jump Delay', function(...)
+    for _75, _75_2 in pairs(getconnections(game:service('UserInputService').JumpRequest))do
+        _75_2.Disable()
+
+        local _ = fenv.A
+    end
+end)
+
+local _call80 = _call26:Sector('Fun')
+
+spawn(function(...)
+    wait(1)
+
+    for _88, _88_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _88_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _96, _96_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _96_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _104, _104_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _104_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _112, _112_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _112_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _120, _120_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _120_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _128, _128_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _128_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _136, _136_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _136_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _144, _144_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _144_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _152, _152_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _152_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _160, _160_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _160_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _168, _168_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _168_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _176, _176_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _176_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _184, _184_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _184_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _192, _192_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _192_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _200, _200_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _200_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _208, _208_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _208_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _216, _216_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _216_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _224, _224_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _224_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _232, _232_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _232_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _240, _240_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _240_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _248, _248_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _248_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _256, _256_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _256_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _264, _264_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _264_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _272, _272_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _272_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _280, _280_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _280_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _288, _288_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _288_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _296, _296_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _296_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _304, _304_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _304_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _312, _312_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _312_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _320, _320_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _320_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _328, _328_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _328_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _336, _336_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _336_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _344, _344_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _344_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _352, _352_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _352_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _360, _360_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _360_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _368, _368_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _368_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _376, _376_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _376_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _384, _384_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _384_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+
+    for _392, _392_2 in pairs(_LocalPlayer4.Character:WaitForChild('Humanoid'):GetPlayingAnimationTracks())do
+        _392_2:AdjustSpeed(1)
+    end
+
+    wait(1)
+    error('internal 579: <25ms: infinitelooperror>')
+end)
+_call80:Cheat('Textbox', 'Animation Speed', function(...) end)
+_call80:Cheat('Button', 'No Flashlight Mesh', function(...)
+    _LocalPlayer4.Backpack:FindFirstChild('Flashlight').Handle.Mesh:destroy()
+end)
+_call80:Cheat('Button', 'No Clothing', function(...) end)
+_call80:Cheat('Button', 'No Hats', function(...) end)
+_call80:Cheat('Button', 'Blockhead', function(...) end)
+_call80:Cheat('Button', 'No Arms', function(...) end)
+_call80:Cheat('Button', 'No Legs', function(...) end)
+
+local _call427 = _call26:Sector('Misc')
+
+_call427:Cheat('Checkbox', 'Full Bright', function(...) end)
+_call427:Cheat('Checkbox', 'Unlock First Person', function(...) end)
+_call427:Cheat('Button', 'Sit', function(...) end)
+_call427:Cheat('Button', 'No Lighting Effects', function(...) end)
+_call427:Cheat('Button', 'Rejoin', function(...) end)
+
+local _call444 = _call12:Category('ESP')
+local _448 = loadstring(game:HttpGet('https://pastebin.com/raw/hjdaYK5p'))()
+
+_448:Toggle(true)
+
+_448.Tracers = false
+_448.Names = false
+_448.Boxes = true
+_448.TeamColor = false
+_448.Players = false
+_448.Color = Color3.fromRGB(255, 255, 255)
+
+local _call454 = _call444:Sector('Location ESP')
+local _Area2Part457 = workspace.Map.ObservationTower.Area2Part
+
+_Area2Part457:IsA('Part')
+
+local _call461 = Instance.new('BillboardGui')
+local _call463 = Instance.new('TextLabel')
+
+_call461.Name = 'esp'
+_call461.Parent = _Area2Part457
+_call461.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+_call461.Active = true
+_call461.AlwaysOnTop = true
+_call461.LightInfluence = 1
+_call461.Size = UDim2.new(0, 200, 0, 50)
+_call463.Name = 'label'
+_call463.Parent = _call461
+_call463.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_call463.BackgroundTransparency = 1
+_call463.Size = UDim2.new(0, 200, 0, 50)
+_call463.Font = Enum.Font.SourceSans
+_call463.Text = 'Observation Tower'
+_call463.TextColor3 = Color3.fromRGB(255, 255, 255)
+_call463.TextSize = 20
+_call463.TextStrokeTransparency = 0
+_call463.TextWrapped = true
+_call463.Visible = false
+
+spawn(function(...) end)
+
+local _ExplosionParts481 = workspace.Map.PowerStation.StationFolder.ExplosionParts
+
+_ExplosionParts481:IsA('Part')
+
+local _call485 = Instance.new('BillboardGui')
+local _call487 = Instance.new('TextLabel')
+
+_call485.Name = 'esp'
+_call485.Parent = _ExplosionParts481
+_call485.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+_call485.Active = true
+_call485.AlwaysOnTop = true
+_call485.LightInfluence = 1
+_call485.Size = UDim2.new(0, 200, 0, 50)
+_call487.Name = 'label'
+_call487.Parent = _call485
+_call487.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_call487.BackgroundTransparency = 1
+_call487.Size = UDim2.new(0, 200, 0, 50)
+_call487.Font = Enum.Font.SourceSans
+_call487.Text = 'Power Station'
+_call487.TextColor3 = Color3.fromRGB(255, 255, 255)
+_call487.TextSize = 20
+_call487.TextStrokeTransparency = 0
+_call487.TextWrapped = true
+_call487.Visible = false
+
+spawn(function(...) end)
+
+local _ShopPart504 = workspace.Map.Shack.ShopPart
+
+_ShopPart504:IsA('Part')
+
+local _call508 = Instance.new('BillboardGui')
+local _call510 = Instance.new('TextLabel')
+
+_call508.Name = 'esp'
+_call508.Parent = _ShopPart504
+_call508.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+_call508.Active = true
+_call508.AlwaysOnTop = true
+_call508.LightInfluence = 1
+_call508.Size = UDim2.new(0, 200, 0, 50)
+_call510.Name = 'label'
+_call510.Parent = _call508
+_call510.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_call510.BackgroundTransparency = 1
+_call510.Size = UDim2.new(0, 200, 0, 50)
+_call510.Font = Enum.Font.SourceSans
+_call510.Text = 'Shop'
+_call510.TextColor3 = Color3.fromRGB(255, 255, 255)
+_call510.TextSize = 20
+_call510.TextStrokeTransparency = 0
+_call510.TextWrapped = true
+_call510.Visible = false
+
+spawn(function(...) end)
+
+local _Door527 = workspace.Map.SafeHouse.Door
+
+_Door527:IsA('Part')
+
+local _call531 = Instance.new('BillboardGui')
+local _call533 = Instance.new('TextLabel')
+
+_call531.Name = 'esp'
+_call531.Parent = _Door527
+_call531.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+_call531.Active = true
+_call531.AlwaysOnTop = true
+_call531.LightInfluence = 1
+_call531.Size = UDim2.new(0, 200, 0, 50)
+_call533.Name = 'label'
+_call533.Parent = _call531
+_call533.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_call533.BackgroundTransparency = 1
+_call533.Size = UDim2.new(0, 200, 0, 50)
+_call533.Font = Enum.Font.SourceSans
+_call533.Text = 'House'
+_call533.TextColor3 = Color3.fromRGB(255, 255, 255)
+_call533.TextSize = 20
+_call533.TextStrokeTransparency = 0
+_call533.TextWrapped = true
+_call533.Visible = false
+
+spawn(function(...) end)
+
+local _Model551 = workspace.Map.BaseCamp.Parts.Model
+
+_Model551:IsA('Part')
+
+local _call555 = Instance.new('BillboardGui')
+local _call557 = Instance.new('TextLabel')
+
+_call555.Name = 'esp'
+_call555.Parent = _Model551
+_call555.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+_call555.Active = true
+_call555.AlwaysOnTop = true
+_call555.LightInfluence = 1
+_call555.Size = UDim2.new(0, 200, 0, 50)
+_call557.Name = 'label'
+_call557.Parent = _call555
+_call557.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_call557.BackgroundTransparency = 1
+_call557.Size = UDim2.new(0, 200, 0, 50)
+_call557.Font = Enum.Font.SourceSans
+_call557.Text = 'Camp'
+_call557.TextColor3 = Color3.fromRGB(255, 255, 255)
+_call557.TextSize = 20
+_call557.TextStrokeTransparency = 0
+_call557.TextWrapped = true
+_call557.Visible = false
+
+spawn(function(...) end)
+_call454:Cheat('Checkbox', 'Tower', function(...) end)
+_call454:Cheat('Checkbox', 'Power Station', function(...) end)
+_call454:Cheat('Checkbox', 'Shop', function(...) end)
+_call454:Cheat('Checkbox', 'House', function(...) end)
+_call454:Cheat('Checkbox', 'Camp', function(...) end)
+
+local _call588 = _call444:Sector('Entity ESP')
+
+_call588:Cheat('Button', 'Rake ESP', function(...) end)
+_call588:Cheat('Checkbox', 'Player ESP', function(...) end)
+_call588:Cheat('Checkbox', 'Tracers', function(...) end)
+_call588:Cheat('Checkbox', 'Names', function(...) end)
+_call588:Cheat('Colorpicker', 'ESP Color', function(...) end)
+
+local _call605 = _call444:Sector('Settings')
+
+_call605:Cheat('Checkbox', 'Flare Gun ESP', function(...) end)
+_call605:Cheat('Checkbox', 'Crate ESP', function(...) end)
+_call605:Cheat('Checkbox', 'Scrap ESP', function(...) end)
+_call605:Cheat('Checkbox', 'Notify when Flare/Crate/Scrap added', function(...) end)
+spawn(function(...) end)
+spawn(function(...) end)
+
+local _call625 = _call12:Category('Credits'):Sector('Credits')
+
+_call625:Cheat('Label', 'Made by Zeerox')
+_call625:Cheat('Label', 'UI - Finity UI Library')
+_call625:Cheat('Label', "Entity ESP - Kiriot22's ESP Library")
